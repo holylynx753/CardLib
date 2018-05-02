0e6eba63b924223fdf96ed687a8dff0687516484
Aleksei Berger <holylynx753@gmail.com>
2018-05-02 22:38:40 +0500

    Добавьте файлы проекта.

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ee22c04c-c798-4d64-bdad-73e9b8e728ef")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ch10CardLib.Tests/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ch10CardLib.Tests
+{
+    [TestFixture]
+    public class DeckTests
+    {
+        [Test, Order(1)]
+        public void getCard()
+        {
+            // Arrange
+            Deck myDeck = new Deck();
+            //myDeck.Shuffle();
+
+            //Act
+            Card tempCard = myDeck.GetCard(0);
+            string cardNameActual = tempCard.ToString();
+
+            Console.WriteLine(cardNameActual);
+
+            //assert 
+            Assert.That(cardNameActual, Is.EqualTo("The Ace of Club's"));
+        }
+
+        [Test, Order(2)]
+        public void get52Card()
+        {
+            // Arrange
+            Deck myDeck = new Deck();
+            myDeck.Shuffle();
+
+            //Act
+            for (int i = 0; i < 52; i++)
+            {
+                Card tempCard = myDeck.GetCard(i);
+                Console.Write(tempCard.ToString());
+                if (i != 51)
+                    Console.Write(", ");
+                else
+                    Console.WriteLine();
+            }
+        }
+
+        [Test, Order(3)]
+        public void AssertThrowsTest()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(
+                new TestDelegate(MethodThatThrows)
+            );
+        }
+
+        void MethodThatThrows()
+        {
+            // Arrange
+            Deck myDeck = new Deck();
+            myDeck.Shuffle();
+
+            //Act
+            Card tempCard = myDeck.GetCard(53);
+            
+        }
+
+    }
+}
